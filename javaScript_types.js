09d9675c2554bfbbbecac75511152ff6fc159231
surya <bittergourd76@gmail.com>
2020-11-02 22:30:34 +0530

    Added Types.js

---
+++ javaScript_types.js
@@ -0,0 +1,28 @@
+//Primitive
+console.log(typeof 5)     //number
+console.log(typeof true)  //boolean
+console.log(typeof 'Hello there') //string
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+console.log(typeof Symbol('Hmm')) //Symbol
+
+/*-------------------------------------------------------------------------------------------*/
+
+//Non-primitive
+console.log(typeof {}) //object
+console.log(typeof function(){}) //object 
+//under the hood function is an object
+//eg:
+function random(){
+    return 1;
+}
+
+random.x = "Voila"
+console.log(random.x)     //Voila
+
+console.log(typeof []) //object
+
+/*-------------------------------------------------------------------------------------------*/
+
+console.log(typeof Math) //object
+console.log(typeof Date) //fucntion
