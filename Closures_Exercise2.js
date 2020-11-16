d759b869272c4d5f22087c93066791b7e6437d4b
surya <bittergourd76@gmail.com>
2020-11-16 22:08:18 +0530

    Added closure exercise2

---
+++ Closures_Exercise2.js
@@ -0,0 +1,29 @@
+const array = [1, 2, 3, 4];
+
+for(var i=0; i < array.length; i++){
+    setTimeout( () => {
+        console.log(i);              //4 4 4 4
+    } , 1000);                            //remember the event loop phases. Timers are executed after poll phase
+}                                               //Here, the execution of for loop would be completed before the execution of Timers
+                
+/*
+1. Once, the callstack gets empty. Timers(in callback queue) are executed, By that time, the value of i would be (n+1). So, the result here is 4 4 4 4. 
+2. Use let(block scoped ) or closures.
+*/
+
+
+//using ES6 feature
+for(let i = 0; i < array.length; i++){
+    setTimeout( () => {
+        console.log(i);                   // 0 1 2 3
+    } , 1000);
+}
+
+
+//using closures
+
+for(var i=0; i < array.length; i++){
+    ( (closureI) => {
+            setTimeout( () => console.log(closureI) , 1000);
+    })(i);
+}
--- The_Two_Pillars_Closures_and_PrototypalInheritance/Exercise_closures.js
+++ The_Two_Pillars_Closures_and_PrototypalInheritance/Exercise_closures.js
@@ -15,3 +15,49 @@ callMe();
     3. then, it will be executed.By that time our callMe function might be popped out of the stack. so, our random varible will be created in memory and marked as closure. 
 
 */
+
+
+//Closures and memory
+
+const heavyDuty = index => {
+    const arr = new Array(7000).fill(1);
+    console.log('Created');
+    return arr[index];
+}
+
+const HeavyDuty = () => {
+    const arr = new Array(7000).fill(1);
+    console.log('Created again!!!');
+    //returning function
+    return index => arr[index];
+}
+
+//array will be created and destroyed each and every time. Not efficient.
+heavyDuty(5);
+heavyDuty(6);
+heavyDuty(1000);
+
+//Efficient code
+const random = HeavyDuty();
+random(4);
+random(5);
+
+//Closures and Encapsulation
+//Here public can't use some data. This function only returns an object containing 2 functions
+const Explode = () => {
+    let timer = 0;
+    const countDown = () => timer++;
+    const launch = () => {
+        timer = 0;
+        return 'test';
+    };
+    setInterval(countDown, 1000);
+    const T = () => timer;
+    return {
+        launch, 
+        T
+    };
+};
+
+const nuclearBomb = Explode();
+nuclearBomb.T();
