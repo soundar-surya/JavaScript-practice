0deece85ac231573bb98d106f346c81b55dbb523
surya <bittergourd76@gmail.com>
2020-12-03 22:25:27 +0530

    Added compose exercise

---
+++ FP/composeExercise1.js
@@ -0,0 +1,18 @@
+//Compose Exercise 1s
+
+Array.prototype.reduceRight = function(...args) {
+    const _this = this;
+    
+    return _this.reverse().reduce(...args);
+}
+
+const compose = (...args) => num => args.reduceRight( (acc, curr) => curr(acc), num )
+
+
+
+const double = num => num * 2
+const inc = num => num + 1
+
+const NumberGame = compose(double, double, double, inc)
+
+console.log(NumberGame(2))
