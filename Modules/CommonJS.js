6cda0b1e306ad4fc3fd6e10693f584deb17739b1
soundar surya <67203625+soundar-surya@users.noreply.github.com>
2020-12-13 15:25:19 +0530

    Update CommonJS.js

--- Modules/CommonJS.js
+++ Modules/CommonJS.js
@@ -1,8 +1,8 @@
 //CommonJS was introduced in node js. It is synchronous.
-//The dependecies will be loaded on the initial server startip. Then, it will be cached in the memory
+//The dependencies will be loaded on the initial server startup. Then, it will be cached in the memory
 
 //Module bundlers are introduced to use CommonJs in browser.
-//Module Bundlers(browserify, weboack, parcel) bundles all th dependecies and code in a single file.which increases the performance
+//Module Bundlers(browserify, weboack, parcel) bundles all th dependencies and code in a single file.which increases the performance
 
 //Example
 
